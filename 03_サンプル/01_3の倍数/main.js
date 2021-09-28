73c0f48963115923c12e36fe852a78467ad84124
jtakei1999 <joe-take@softbank.ne.jp>
2021-09-29 06:30:10 +0900

    3の倍数

--- 03_サンプル/01_3の倍数/main.js
+++ 03_サンプル/01_3の倍数/main.js
@@ -1,11 +1,22 @@
+// n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
+// n を 3 で割ったあまりが 0
 const number = 24
 
-// n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
-for (let n = 1; n <= number; n++) {
-  // n を 3 で割ったあまりが 0
-  if (n % 3 === 0) {
-    console.log(`${n}!!!!!!!`)
+for (let i = 1; i <= number; i++) {
+  if (i % 3 === 0) {
+    console.log(i + "!!!!")
   } else {
-    console.log(n)
+    console.log(i)
   }
 }
+
+const fineNumber = function(number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0) {
+      console.log(i + "!!!!")
+    } else {
+      console.log(i)
+    }
+  }
+}
+fineNumber(30)
