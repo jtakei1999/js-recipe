4d2fdc1dedc8a7dbfcf14cdb90ebd43d4ecfc603
jtakei1999 <joe-take@softbank.ne.jp>
2021-09-30 02:10:42 +0900

    □と○

--- 03_サンプル/01_□と○/main.js
+++ 03_サンプル/01_□と○/main.js
@@ -1,5 +1,23 @@
-const figure = document.getElementById("figure")
+/*const figure = document.getElementById("figure")
 
 figure.onclick = function() {
   figure.classList.toggle("rounded")
+}*/
+
+/*const figure = document.getElementById("figure")
+
+figure.onclick = function() {
+  figure.classList.toggle("rounded")
+}
+*/
+
+const figure = document.getElementById("figure")
+const circleButton = document.getElementById("circle-button")
+const squareButton = document.getElementById("square-button")
+
+circleButton.onclick = function() {
+  figure.classList.add("rounded")
+}
+squareButton.onclick = function() {
+  figure.classList.remove("rounded")
 }
