341be1bc1b4ef3781a6d2ae6651734fe19affc00
jtakei1999 <joe-take@softbank.ne.jp>
2021-10-15 00:46:31 +0900

    catAPI

--- 05_practice/02_CatAPI/main.js
+++ 05_practice/02_CatAPI/main.js
@@ -1,9 +1,10 @@
 const imageElement = document.getElementById("cat-image")
 
-fetch("https://api.thecatapi.com/v1/images/search?breed_ids= {breed-id}")
+fetch("https://api.thecatapi.com/v1/images/search")
   .then((res) => {
     return res.json()
   })
   .then((data) => {
-    imageElement.src = data.message
+    console.dir(data)
+    imageElement.src = data[0].url
   })
